e68d2b6db4d0112158d756eb7e45c060affb49ad
wonderfulMan <1259243840@qq.com>
2020-07-07 16:10:43 +0800

    fix bug

--- generator.js
+++ generator.js
@@ -68,8 +68,7 @@ module.exports = (api, options, rootOptions) => {
 
   api.render('./templates/mobile')
   // 无效
-  api.render('./templates/mobile/.vscode')
-  api.render('./templates/mobile/script')
+  api.render('./templates/mobile/scripts')
   // 上面的方式不能拷贝隐藏文件
   api.render({
     '.browserslistrc': './templates/mobile/.browserslistrc',

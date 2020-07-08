df82d450f1da7dc3115b26c2a15cb7d03af63bd2
wonderfulMan <1259243840@qq.com>
2020-07-08 13:34:07 +0800

    fix bug

--- templates/mobile/.eslintrc.js
+++ templates/mobile/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:zx/custom", "standard"],
   parserOptions: {
     parser: "babel-eslint"
   },

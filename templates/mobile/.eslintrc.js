b8a0b582040e271cd1033f44e0287ca3dbc881b2
zhouxiang1991 <zhouxiang219@163.com>
2020-07-17 15:23:36 +0800

    调整eslint规则，目的是去除?.()操作提示的no-unused-expressions错误提示

--- templates/mobile/.eslintrc.js
+++ templates/mobile/.eslintrc.js
@@ -14,6 +14,8 @@ module.exports = {
   rules: {
     'zx/comma-spacing': 0,
     'zx/component-name-hyphenation': 0,
+    'no-unused-expressions': 0,
+    'babel/no-unused-expressions': 1,
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
